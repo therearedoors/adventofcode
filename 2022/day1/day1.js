53eba5c3bf3a2421a3db2be31db95d95c5948adb
Paul S <paul.smart@firstoptionsoftware.com>
2022-12-01 05:47:11 +0000

    day1

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -2,12 +2,41 @@ const fs = require('fs')
 
 const data = () => fs.readFileSync('../input1.txt',{encoding: 'utf8'})
 const trimData = () => data().trim();
-const lines = () => trimData().split('\n');
-const numbers = () => lines().map(n=>+n);
 
-const splitSpace = s => s.split(' ');
-const split = s => s.split('');
-const filterStr = (s,f) => splitSpace(s).filter(f).join('');
 const sum = arr => arr.reduce((a,b)=>a+b);
-const product = arr => arr.reduce((a,b)=>a*b);
 const log = any => console.log(any);
+
+//PART ONE
+let elves = trimData().split('\n\n').map(c => c.trim().split('\n')).map(arr=>arr.map(n => +n)).map(arr => sum(arr))
+
+log(elves.reduce((a,b)=>{
+    if (a > b) return a;
+    if (b > a) return b;
+}));
+
+// PART TWO
+let one = 0
+let two = 0
+let three = 0
+
+for (let i=0;i<elves.length;i++){
+    if (elves[i] > one){
+        two = one;
+        one = elves[i]
+        continue;
+    }
+    if (elves[i] > two){
+        three = two;
+        two = elves[i];
+        continue;
+    }
+    if (elves[i] > three){
+        three = elves[i];
+    }
+}
+
+log(one)
+log(two)
+log(three)
+log(one+two+three)
+
